b4b94759e51955eaf94405da8390233d10119aca
CJBrew <chrisyate@gmail.com>
2011-09-12 23:40:32 +0100

    Updated ui.py to search regex and use that to specify colours.
    Colors are specified in colorpatterns.py, with some examples.
    
    Style is compulsory (?!) but color can be specified with only forecolor or both.

--- noobhack/colorpatterns.py
+++ noobhack/colorpatterns.py
@@ -1,6 +1,11 @@
 patterns = {
-    " blessed ": ("bold", "green"),
-    " uncursed " : ("dim", "yellow"),
-    " cursed " : ("bold", "red", "yellow"),
-    "^cloak of displacement" : ("underline", "green", "cyan")
+# styles: bold, dim, underline, blink, reverse
+# colors: black, blue, cyan, green, magenta, red, white, yellow, brown, default
+# format: "regex" (assume pre- and post- fixed with /b) : (style, [forecolor, [backcolor]])
+    "blessed": ("bold", "green"),
+    "uncursed" : ("dim", "yellow"),
+    "cursed" : ("bold", "red", "yellow"),
+    "cloak of displacement" : ("underline", "black", "cyan"),
+    "[0-9a]+ potion[s]? of gain level" : ("blink", ), # note single length tuple requires a comma 
+    "[0-9a]+ potion[s]? of gain energy" : ("reverse",)
 }
