762df82549fac8597c1be351bb84ec7cc49d8470
CJBrew <chrisyate@gmail.com>
2011-09-13 00:17:20 +0100

    Small changes to ui.py and colorpatterns.py to tidy up

--- noobhack/colorpatterns.py
+++ noobhack/colorpatterns.py
@@ -1,11 +1,13 @@
 patterns = {
+# format: "regex" (assume pre- and post- fixed with /b) : (style, [forecolor, [backcolor]])
 # styles: bold, dim, underline, blink, reverse
 # colors: black, blue, cyan, green, magenta, red, white, yellow, brown, default
-# format: "regex" (assume pre- and post- fixed with /b) : (style, [forecolor, [backcolor]])
     "blessed": ("bold", "green"),
     "uncursed" : ("dim", "yellow"),
-    "cursed" : ("bold", "red", "yellow"),
-    "cloak of displacement" : ("underline", "black", "cyan"),
-    "[0-9a]+ potion[s]? of gain level" : ("blink", ), # note single length tuple requires a comma 
-    "[0-9a]+ potion[s]? of gain energy" : ("reverse",)
+    "cursed" : ("bold", "red"),
+# Some more examples...
+# "cursed [+-][0-9] dwarvish mattock" : ("bold", "red", "yellow"),
+# "cloak of displacement" : ("underline", "black", "cyan"),
+#    "[0-9a]+ potion[s]? of gain level" : ("blink", ), # note single length tuple requires a comma 
+#    "[0-9a]+ potion[s]? of gain energy" : ("reverse",)
 }
