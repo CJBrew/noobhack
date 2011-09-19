46e4d0bcf41840419c618568d017a5bb4f9be8bc
CJBrew <chrisyate@gmail.com>
2011-09-19 23:12:41 +0100

    added new tests

--- test/game/test_intrinsics.py
+++ test/game/test_intrinsics.py
@@ -10,24 +10,57 @@ testIntrinsics = {
     }
 
 class IntrinsicsTest(unittest.TestCase):
-#  def test_nullTestFail(self):
-#	self.assertEqual(1, 0)
-#
-#  def test_nullTestPass(self):
-#	self.assertEqual(1,1)
 
-	def test_parse(self):
+	def test_parseOnlyNoResult(self):
 	  testEmpty = ""
 	  uTestEmpty = testEmpty.decode('utf-8')
 	  res = intrinsicsC.DispatchIntrinsics(testIntrinsics, [uTestEmpty])
+	  self.assertEqual(len(res), 0)
 
-	def test_found_valA(self):
+	def test_found_valA_Enable(self):
 	  testInput = "blah aaa blah"
 	  uTestInput = testInput.decode('utf-8')
 	  res = intrinsicsC.DispatchIntrinsics(testIntrinsics, [uTestInput])
 	  self.assertEqual(len(res), 1)
 	  self.assertEqual(res.items()[0][0], "valA")
 	  self.assertEqual(res.items()[0][1], True)
+
+	def test_found_valA_Disable(self):
+	  testInput = "blah aaZ blah"
+	  uTestInput = testInput.decode('utf-8')
+	  res = intrinsicsC.DispatchIntrinsics(testIntrinsics, [uTestInput])
+	  self.assertEqual(len(res), 1)
+	  self.assertEqual(res.items()[0][0], "valA")
+	  self.assertEqual(res.items()[0][1], False)
+	
+	def test_found_valA_EnableDisable(self):
+	  testInput = "aaa blah aaZ blah"
+	  uTestInput = testInput.decode('utf-8')
+	  res = intrinsicsC.DispatchIntrinsics(testIntrinsics, [uTestInput])
+	  self.assertEqual(len(res), 1)
+	  self.assertEqual(res.items()[0][0], "valA")
+	  self.assertEqual(res.items()[0][1], False)
+
+	def test_found_valAvalB_Enabled(self):
+	  testInput = "aaa blah bbb blah"
+	  uTestInput = testInput.decode('utf-8')
+	  res = intrinsicsC.DispatchIntrinsics(testIntrinsics, [uTestInput])
+	  self.assertEqual(len(res), 2)
+	  self.assertEqual(res.items()[0][0], "valB")
+	  self.assertEqual(res.items()[0][1], True)
+	  self.assertEqual(res.items()[1][0], "valA")
+	  self.assertEqual(res.items()[1][1], True)
+
+	def test_found_valBvalA_Enabled(self):
+	  testInput = "bbb blah aaa blah"
+	  uTestInput = testInput.decode('utf-8')
+	  res = intrinsicsC.DispatchIntrinsics(testIntrinsics, [uTestInput])
+	  self.assertEqual(len(res), 2)
+	  self.assertEqual(res.items()[0][0], "valB")
+	  self.assertEqual(res.items()[0][1], True)
+	  self.assertEqual(res.items()[1][0], "valA")
+	  self.assertEqual(res.items()[1][1], True)
+
 
 if __name__ == "__main__":
   unittest.main()
