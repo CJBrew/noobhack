e142574b38e669afdd7ed947466c17c4cff9661c
Chris Yate <chrisyate@gmail.com>
2011-10-07 22:11:02 +0100

    intrinsics.cpp : Using Boost::regex to search in C++ for intrinsics.
    intrinsics.py : Since we're now looking at regex search we don't want full stops in the search strings - removed. Also removed are exclamation marks.
    
    setup.py : modified the Extensions build info to link against libboost_regex

--- noobhack/game/intrinsics.py
+++ noobhack/game/intrinsics.py
@@ -1,42 +1,42 @@
 messages = {
     "Warning": {
-        "You feel sensitive!":True,
-        "You feel less sensitive!":False,
+        "You feel sensitive":True,
+        "You feel less sensitive":False,
      },
     "Shock resistance": {
-        "Your health currently feels amplified!":True,
-        "You feel insulated!":True,
+        "Your health currently feels amplified":True,
+        "You feel insulated":True,
         "You are shock resistant":True,
-        "You feel grounded in reality.":True,
+        "You feel grounded in reality":True,
         "You feel conductive":False
     },
     "Fire resistance": {
-        "You be chillin'.":True,
-        "You feel cool!":True,
+        "You be chillin'":True,
+        "You feel cool":True,
         "You are fire resistant":True,
-        "You feel a momentary chill.":True,
-        "You feel warmer!":False
+        "You feel a momentary chill":True,
+        "You feel warmer":False
     },
     "Cold resistance": {
         "You are cold resistant":True,
-        "You feel warm!":True,
-        "You feel full of hot air.":True,
-        "You feel cooler!":False
+        "You feel warm":True,
+        "You feel full of hot air":True,
+        "You feel cooler":False
     },
-    "Disintegration resist.": {
+    "Disintegration resist": {
         "You are disintegration-resistant":True,
-        "You feel very firm.":True,
-        "You feel totally together, man.":True        
+        "You feel very firm":True,
+        "You feel totally together, man":True        
     },
     "Poison resistance": {
         "You are poison resistant":True,
         "You feel( especially)? (healthy)|(hardy)":True,
-        "You feel a little sick!":False
+        "You feel a little sick":False
     },
     "Sleep resistance": {
         "You are sleep resistant":True,
         "You feel( wide)? awake":True,
-        "You feel tired!":False
+        "You feel tired":False
     },
     "Aggravate monster": {
         "You feel that monsters are aware of your presence":True,
@@ -46,22 +46,22 @@ messages = {
         "You feel vulnerable":False
     },
     "Invisible": {
-        "You feel hidden!":True
+        "You feel hidden":True
     },
     "See invisible": {
-        "You see an image of someone stalking you.":True,
+        "You see an image of someone stalking you":True,
         "You feel transparent":True,
         "You feel very self-conscious":True, 
         "Your vision becomes clear":True
     },
     "Searching": {
-        "You feel perceptive!":True
+        "You feel perceptive":True
     },
     "Speed": {
-        "You feel quick!":True,
-        "You feel yourself speed up.":True, # speed boots put on (want this here?)!
-        "You feel yourself slow down.":False, # speed boots removed (want this here?)!
-        "You feel slow!":False
+        "You feel quick":True,
+        "You feel yourself speed up":True, # speed boots put on (want this here?)!
+        "You feel yourself slow down":False, # speed boots removed (want this here?)!
+        "You feel slow":False
     },
     "Teleportitis": {
        "You feel very jumpy":True,
@@ -70,9 +70,9 @@ messages = {
     },
     "Teleport control": {
         "You feel in control of yourself":True,
-        "You feel controlled!":True,
+        "You feel controlled":True,
         "You feel centered in your personal space":True,
-       "You feel less jumpy":False
+        "You feel less jumpy":False
     },
     "Telepathy": {
         "You feel in touch with the cosmos":True,
